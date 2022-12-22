99a49766d4eefb7fb5883d9898101a75dde35e56
lesmon <lesmonnn@gmail.com>
2022-12-22 18:18:16 +0800

    banner

--- react-app-netflix-redux/src/Banner.js
+++ react-app-netflix-redux/src/Banner.js
@@ -2,12 +2,16 @@ import React from "react";
 import "./Banner.css";
 
 function Banner() {
+  function truncate(string, n) {
+    return string?.length > n ? string.substr(0, n - 1) + "..." : string;
+  }
+
   return (
     <header
       className="banner"
       style={{
         backgroundSize: "cover",
-        backgroundImage: `url("https://webpages.scu.edu/ftp/lrlam/Images/Netflix_banner.jpg")`,
+        backgroundImage: `url("http://getwallpapers.com/wallpaper/full/2/6/6/29787.jpg")`,
         backgroundPosition: "center center",
       }}>
       <div className="banner__contents">
@@ -16,8 +20,23 @@ function Banner() {
           <button className="banner__button">play</button>
           <button className="banner__button">my list</button>
         </div>
-        <h1 className="banner__description">This is a test description.</h1>
+        <h1 className="banner__description">
+          {truncate(
+            `This is a test description. This is a test description. This is a test
+          description. This is a test description.This is a test description.
+          This is a test description. This is a test description. This is a test
+          description. This is a test description.This is a test description.
+          This is a test description. This is a test description. This is a test
+          description. This is a test description. This is a test description.
+          This is a test description. This is a test description. This is a test
+          description. This is a test description. This is a test.
+          This is a test description.`,
+            20
+          )}
+        </h1>
       </div>
+
+      <div className="banner__fadeBottom"></div>
     </header>
   );
 }
