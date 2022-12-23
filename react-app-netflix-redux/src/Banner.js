96fb57231c01238fbf753be7b6cfc75ce611e3be
lesmon <lesmonnn@gmail.com>
2022-12-23 19:55:51 +0800

    up

--- react-app-netflix-redux/src/Banner.js
+++ react-app-netflix-redux/src/Banner.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Banner.css";
+import axios from "./axios";
+import requests from "./Requests";
 
 function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+    }
+    fetchData();
+  }, []);
+
+  console.log(movie);
+
   function truncate(string, n) {
     return string?.length > n ? string.substr(0, n - 1) + "..." : string;
   }
@@ -11,32 +29,23 @@ function Banner() {
       className="banner"
       style={{
         backgroundSize: "cover",
-        backgroundImage: `url("http://getwallpapers.com/wallpaper/full/2/6/6/29787.jpg")`,
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
         backgroundPosition: "center center",
       }}>
       <div className="banner__contents">
-        <h1 className="banner__title">Movie Title</h1>
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
         <div className="banner__buttons">
           <button className="banner__button">play</button>
           <button className="banner__button">my list</button>
         </div>
         <h1 className="banner__description">
-          {truncate(
-            `This is a test description. This is a test description. This is a test
-          description. This is a test description.This is a test description.
-          This is a test description. This is a test description. This is a test
-          description. This is a test description.This is a test description.
-          This is a test description. This is a test description. This is a test
-          description. This is a test description. This is a test description.
-          This is a test description. This is a test description. This is a test
-          description. This is a test description. This is a test.
-          This is a test description.`,
-            20
-          )}
+          {truncate(movie?.overview, 150)}
         </h1>
       </div>
 
-      <div className="banner__fadeBottom"></div>
+      <div className="banner__fadeBottom" />
     </header>
   );
 }
